15ffc97ca7cac4759b79db46ada3bf234ee58eff
Napoleon-Christos Oikonomou <Napoleonoikon@gmail.com>
2017-11-14 01:56:40 +0200

    problem 3

---
+++ app/3.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = () => {
+  let factor;
+  let start = 600851475143;
+  for (let i = 3; i < start + 1; i ++) {
+    if (start % i === 0 && isPrime(i)) {
+      start /= i;
+      factor = i;
+    }
+  }
+  return console.log('Problem 3 solution is: ' + factor);
+};
+
+function isPrime(number) {
+  for (let i = 2, s = Math.sqrt(number); i <= s; i++) {
+    if (number % i === 0) return false;
+  }
+  return number !== 1;
+}
--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 
 const p1 = require('./app/1'); p1();
 const p2 = require('./app/2'); p2();
+const p3 = require('./app/3'); p3();
