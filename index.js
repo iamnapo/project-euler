5ee18b6d3da56f40bfc22de1276c4401f29f6a47
Napoleon-Christos Oikonomou <Napoleonoikon@gmail.com>
2017-11-14 02:33:30 +0200

    problem 6

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const p2 = require('./app/2'); p2();
 const p3 = require('./app/3'); p3();
 const p4 = require('./app/4'); p4();
 const p5 = require('./app/5'); p5();
+const p6 = require('./app/6'); p6();
