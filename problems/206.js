27ef2df52341f7f7f94f7d73dab852099fa72db5
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2021-11-11 13:40:22 +0200

    update eslint

--- problems/206.js
+++ problems/206.js
@@ -1,5 +1,5 @@
 export default () => {
-	const pattern = new RegExp(/1.2.3.4.5.6.7.8.9.0/);
+	const pattern = /1.2.3.4.5.6.7.8.9.0/;
 	for (let i = 1_010_101_010n; i < 1_389_026_623n; i += 10n) {
 		if (pattern.test(i ** 2n)) return `Problem 206 solution is: ${i}`;
 	}
